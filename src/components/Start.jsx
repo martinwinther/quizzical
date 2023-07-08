acd53c7d68a04eb998c931cf98a7cefea2ae5e3b
Martin Winther <mswinther@icloud.com>
2023-07-08 14:43:16 +0200

    css

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -2,8 +2,8 @@
 export default function Start(props) {
 	return (
 		<div>
-			<h1>Quizzical</h1>
-			<p>You know what to do</p>
+			<h1 className="title">Quizzical</h1>
+			<p className="hero">You know what to do</p>
 			<button className="start-btn" onClick={props.start}>
 				Start quiz
 			</button>
