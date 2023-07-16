166e6f1c5957af2b76863d12f84faf7bc424a199
Martin Winther <mswinther@icloud.com>
2023-07-16 12:15:12 +0200

    note

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,53 +2,7 @@ export default function Quiz() {
 	return (
 		<main>
 			<div className="question-container">
-				<h3>Question</h3>
-				<ul>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-				</ul>
-				<hr />
-			</div>
-			<div className="question-container">
-				<h3>Question</h3>
-				<ul>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-				</ul>
-				<hr />
-			</div>
-			<div className="question-container">
-				<h3>Question</h3>
-				<ul>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-				</ul>
-				<hr />
-			</div>
-			<div className="question-container">
-				<h3>Question</h3>
-				<ul>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-				</ul>
-				<hr />
-			</div>
-			<div className="question-container">
-				<h3>Question</h3>
-				<ul>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-					<li>Answer</li>
-				</ul>
+				{/* Make this with radio buttons */}
 				<hr />
 			</div>
 			<p>You scored 3/5 correct answers</p>
